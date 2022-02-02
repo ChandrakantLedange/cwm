0aa343f8e0eee776481e4c2dc2e086dd903af8d2
Chandrakant Ledange <43463105+ChandrakantLedange@users.noreply.github.com>
2022-02-02 23:21:16 +0530

    passing data to components

--- counter-app/src/components/Counter.js
+++ counter-app/src/components/Counter.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export default class Counter extends Component {
     state ={ 
-        count:0,
+        value:this.props.value,
         tags:[]
     };
     // constructor
@@ -29,7 +29,7 @@ export default class Counter extends Component {
     handleIncrement=(product)=>{
         console.log(product);
         this.setState({
-            count: this.state.count +1
+            value: this.state.value +1
         })
     }
     //pass event argument there are two ways
@@ -39,7 +39,7 @@ export default class Counter extends Component {
     // }
     //2.call main method from event with arrow function paasing an argument
   render() { 
-    
+    console.log("porps",this.props);
     return (
         <React.Fragment>
             {/* dynamic css */}
@@ -57,13 +57,13 @@ export default class Counter extends Component {
   //Dynamic classess
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
   formatCount(){
-      const {count} = this.state;
-      return count === 0 ? 'ZERO' : count;
+      const {value} = this.state;
+      return value === 0 ? 'ZERO' : value;
   }
 }
 
